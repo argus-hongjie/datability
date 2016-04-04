bea98ecd1829df6f9da2069bb36c0e09a0d482e1
Thomas Queste <tom@tomsquest.com>
2016-04-04 13:28:21 +0200

    Drop primary keys by reading `table_constraints` instead of indexes

--- src/main/java/datability/Postgresql.java
+++ src/main/java/datability/Postgresql.java
@@ -42,7 +42,7 @@ public class Postgresql implements Database {
         if (tables != null) {
             try {
                 for (String table : tables) {
-                    for (String primaryKey : findIndexes(table)) {
+                    for (String primaryKey : findPrimaryKeyConstraints(table)) {
                         LOG.info("Disabling primary key '{}' of table '{}'", primaryKey, table);
                         executeSql("ALTER TABLE " + table + " DROP CONSTRAINT " + primaryKey);
                     }
@@ -60,7 +60,7 @@ public class Postgresql implements Database {
         if (tables != null) {
             try {
                 for (String table : tables) {
-                    for (String foreignKey : findForeignKeys(table)) {
+                    for (String foreignKey : findForeignKeyConstraints(table)) {
                         LOG.info("Disabling foreign key '{}' of table '{}'", foreignKey, table);
                         executeSql("ALTER TABLE " + table + " DROP CONSTRAINT " + foreignKey);
                     }
@@ -84,7 +84,7 @@ public class Postgresql implements Database {
     }
 
     private List<String> findNotNullColumns(String table) throws SQLException {
-        List<String> notNullsColumns = new ArrayList<String>();
+        List<String> notNullsColumns = new ArrayList<>();
 
         String sql = "SELECT DISTINCT column_name FROM INFORMATION_SCHEMA.COLUMNS" +
                 " WHERE column_name IS NOT NULL" +
@@ -98,27 +98,26 @@ public class Postgresql implements Database {
         }
 
         // Cannot drop-non-null from columns part of a primary key
-        notNullsColumns.removeAll(findPrimaryKeys(table));
+        notNullsColumns.removeAll(findPrimaryKeyColumns(table));
 
         return notNullsColumns;
     }
 
-    private List<String> findPrimaryKeys(String table) throws SQLException {
+    private List<String> findPrimaryKeyColumns(String table) throws SQLException {
         return getFirstColumnValues("SELECT a.attname" +
                 " FROM pg_index i JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY (i.indkey)" +
                 " WHERE i.indrelid = '" + table + "'::REGCLASS AND i.indisprimary");
 
     }
 
-    private List<String> findIndexes(String table) throws SQLException {
-        return getFirstColumnValues("SELECT i.relname as index_name" +
-                " FROM pg_class t JOIN pg_attribute a ON a.attrelid = t.oid" +
-                "  JOIN pg_index ix ON t.oid = ix.indrelid AND a.attnum = ANY(ix.indkey)" +
-                "  JOIN pg_class i ON i.oid = ix.indexrelid" +
-                " WHERE t.relkind = 'r' AND t.relname in ('" + table + "')");
+    private List<String> findPrimaryKeyConstraints(String table) throws SQLException {
+        return getFirstColumnValues("SELECT constraint_name" +
+                " FROM information_schema.table_constraints" +
+                " WHERE constraint_type = 'PRIMARY KEY'" +
+                " AND table_name='" + table + "'");
     }
 
-    private List<String> findForeignKeys(String table) throws SQLException {
+    private List<String> findForeignKeyConstraints(String table) throws SQLException {
         return getFirstColumnValues("SELECT constraint_name" +
                 " FROM information_schema.table_constraints" +
                 " WHERE constraint_type = 'FOREIGN KEY'" +
@@ -126,7 +125,7 @@ public class Postgresql implements Database {
     }
 
     private List<String> getFirstColumnValues(String sql) throws SQLException {
-        List<String> values = new ArrayList<String>();
+        List<String> values = new ArrayList<>();
         ResultSet rs = connection.createStatement().executeQuery(sql);
         while (rs.next()) {
             values.add(rs.getString(1));
@@ -136,6 +135,7 @@ public class Postgresql implements Database {
 
     private void executeSql(String sql) {
         try {
+            LOG.debug("Executing sql: {}", sql);
             connection.createStatement().execute(sql);
         } catch (SQLException e) {
             throw new DatabaseException(sql, e);
