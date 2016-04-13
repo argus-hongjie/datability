273ed8d96a23f84770b3fdce117e0eab67f860f5
Thomas Queste <tom@tomsquest.com>
2016-04-13 16:24:39 +0200

    Make `Databases` not instantiable

--- src/main/java/datability/Databases.java
+++ src/main/java/datability/Databases.java
@@ -3,6 +3,10 @@ package datability;
 import java.sql.Connection;
 
 public class Databases {
+
+    private Databases() {
+    }
+
     public static Database postgresql(Connection connection) {
         return new Postgresql(connection);
     }
