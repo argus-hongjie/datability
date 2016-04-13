318ed5e24c223f586de85ba5ef2a0a6025868c25
Thomas Queste <tom@tomsquest.com>
2016-04-13 16:40:39 +0200

    Make `Assert` not instantiable

--- src/main/java/datability/Assert.java
+++ src/main/java/datability/Assert.java
@@ -3,6 +3,10 @@ package datability;
 import java.sql.Connection;
 
 public class Assert {
+
+    private Assert() {
+    }
+
     public static <T> T notNull(T ref, String errorMsg) {
         if (ref == null) {
             throw new IllegalArgumentException(errorMsg);
